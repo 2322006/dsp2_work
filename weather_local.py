6769d811cba4c04467ba9fa62f5d5765bbbef3ca
2322006 <s2322006@stu.musashino-u.ac.jp>
2024-01-14 22:18:14 +0900

    time列を追加

--- weather_local.py
+++ weather_local.py
@@ -18,7 +18,7 @@ cur.execute('DROP TABLE IF EXISTS weather_local_db;')
 # 実行したいSQLを用意する
 # テーブルを作成するSQL
 # CREATE TABLE テーブル名（カラム名 型，...）;
-sql_create_table_weather_local_db = 'CREATE TABLE weather_local_db(pressure_local int, temperature_local int, humidity_local int);'
+sql_create_table_weather_local_db = 'CREATE TABLE weather_local_db(time INTEGER, pressure_local int, temperature_local int, humidity_local int);'
 
 # SQLを実行する
 cur.execute(sql_create_table_weather_local_db)
@@ -26,34 +26,34 @@ cur.execute(sql_create_table_weather_local_db)
 # SQLを用意
 # データを挿入するSQL
 # INSERT INTO テーブル名 VALUES (列に対応したデータをカンマ区切りで);
-sql_insert_many = "INSERT INTO weather_local_db VALUES (?, ?, ?);"
+sql_insert_many = "INSERT INTO weather_local_db VALUES (?, ?, ?, ?);"
 
 # データをリストで用意する
 weather_local_db_list = [
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
-    (0, 0, 0),
+    (1, 0, 0, 0),
+    (2, 0, 0, 0),
+    (3, 0, 0, 0),
+    (4, 0, 0, 0),
+    (5, 0, 0, 0),
+    (6, 0, 0, 0),
+    (7, 0, 0, 0),
+    (8, 0, 0, 0),
+    (9, 0, 0, 0),
+    (10, 0, 0, 0),
+    (11, 0, 0, 0),
+    (12, 0, 0, 0),
+    (13, 0, 0, 0),
+    (14, 0, 0, 0),
+    (15, 0, 0, 0),
+    (16, 0, 0, 0),
+    (17, 0, 0, 0),
+    (18, 0, 0, 0),
+    (19, 0, 0, 0),
+    (20, 0, 0, 0),
+    (21, 0, 0, 0),
+    (22, 0, 0, 0),
+    (23, 0, 0, 0),
+    (24, 0, 0, 0),
 ]
 
 # SQLを実行
